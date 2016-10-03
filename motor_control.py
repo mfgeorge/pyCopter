158799fe9fda8bd60343cf47089d09b4004ae741
ogiovanniruiz <ogiovanniruiz@gmail.com>
2016-10-03 12:31:09 -0700

    Main and motor_control are ready!

--- motor_control.py
+++ motor_control.py
@@ -24,20 +24,3 @@ class Motor_Control:
         self.servo3.speed(ss3)
 
         self.servo4.speed(ss4)
-
-def main():
-    ss1 = 50
-    ss2 = 0
-    ss3 = 0
-    ss4 = 0
-
-    servo1 = pyb.Servo(1)
-    servo2 = pyb.Servo(2)
-    servo3 = pyb.Servo(3)
-    servo4 = pyb.Servo(4)
-
-    controller = Motor_Control(servo1, servo2, servo3, servo4)
-    controller.motor_task(ss1, ss2, ss3, ss4)
-
-if __name__ == '__main__':
-    main()
