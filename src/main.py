967ff8748a89c80ff3861cd38fab1af855aa9e37
Chad <clbickel@calpoly.edu>
2016-10-05 17:06:30 -0700

    Added IMU library and changed main a bit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 from pyb import Servo
 from motor_control import Motor_Control
+from BNO055_lib import BNO055
 
 def main():
     ss1 = 50
@@ -15,6 +16,8 @@ def main():
     servo4 = pyb.Servo(4)
 
     controller = Motor_Control(servo1, servo2, servo3, servo4)
+
+    IMU = BNO055()
     while True:
         try:
             speeds = tuple(map(int,input("\n Enter Motor Speeds in format ss1,ss2,ss3,ss4 \n").split(',')))
@@ -26,6 +29,10 @@ def main():
         except IndexError:
             print(" \n \n Please provide at least 4 input paramters for all motors \n \n")
             pass
+
+        pitch = IMU.get_pitch()
+        roll = IMU.get_roll()
+        yaw = IMU.get_yaw()
 
 if __name__ == '__main__':
     main()
