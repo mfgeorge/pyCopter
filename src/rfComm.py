dc00f008246288533c46c4ff464f04d8c3259ff6
Michael George <michaelgeorge180@gmail.com>
2016-10-10 10:15:54 -0700

    fixed the initial errors in the rfComm code. Now working on an OSError:16 thrown when an interrupt is not handeled.

--- src/rfComm.py
+++ src/rfComm.py
@@ -33,7 +33,7 @@ class RFController6CH:
 class SpektrumController(RFController6CH):
     'A class for a specific 6 channel Spektrum radio module.'
 
-	def __init__(self, thro_pin, aile_pin, elev_pin, rudd_pin, gear_pin, aux1_pin):
+    def __init__(self, thro_pin, aile_pin, elev_pin, rudd_pin, gear_pin, aux1_pin):
         self.thro_pulse = ServoPulse(thro_pin)
         self.aile_pulse = ServoPulse(aile_pin)
         self.elev_pulse = ServoPulse(elev_pin)
@@ -69,13 +69,14 @@ class ServoPulse:
     start = width = last_width = 0
 
     def __init__(self, pin):
-        timer_number, timer_channel_number = TimerHelper.get_timer(pin)
+        timer_dict = TimerHelper()
+        timer_number, timer_channel_number = timer_dict.get_timer(pin)
         timer = pyb.Timer(timer_number, prescaler=83, period=0x0fffffff)
         self.pin = pin
         self.channel = timer.channel(timer_channel_number,
-                                    Timer.IC,
+                                    pyb.Timer.IC,
                                     pin=self.pin,
-                                    polarity=Timer.BOTH)
+                                    polarity=pyb.Timer.BOTH)
         self.channel.callback(self.callback)
 
     def callback(self, timer):
@@ -99,42 +100,42 @@ class TimerHelper:
     instantiation of this class.
     """
     timer_dict = {}
-    timer_dict[pyb.Pin.board.Y1] = (8)
-    timer_dict[pyb.Pin.board.Y2] = (8)
-    timer_dict[pyb.Pin.board.Y3] = (4, 10)
-    timer_dict[pyb.Pin.board.Y4] = (4, 11)
-    timer_dict[pyb.Pin.board.Y5] = None
-    timer_dict[pyb.Pin.board.Y6] = (1)
-    timer_dict[pyb.Pin.board.Y7] = (1, 8, 12)
-    timer_dict[pyb.Pin.board.Y8] = (1, 8, 12)
-    timer_dict[pyb.Pin.board.Y9] = (2)
-    timer_dict[pyb.Pin.board.Y10] = (2)
-    timer_dict[pyb.Pin.board.Y11] = (1, 8)
-    timer_dict[pyb.Pin.board.Y12] = (1, 8)
+    timer_dict[str(pyb.Pin.board.Y1)] = (8)
+    timer_dict[str(pyb.Pin.board.Y2)] = (8)
+    timer_dict[str(pyb.Pin.board.Y3)] = (4, 10)
+    timer_dict[str(pyb.Pin.board.Y4)] = (4, 11)
+    timer_dict[str(pyb.Pin.board.Y5)] = None
+    timer_dict[str(pyb.Pin.board.Y6)] = (1)
+    timer_dict[str(pyb.Pin.board.Y7)] = (1, 8, 12)
+    timer_dict[str(pyb.Pin.board.Y8)] = (1, 8, 12)
+    timer_dict[str(pyb.Pin.board.Y9)] = (2)
+    timer_dict[str(pyb.Pin.board.Y10)] = (2)
+    timer_dict[str(pyb.Pin.board.Y11)] = (1, 8)
+    timer_dict[str(pyb.Pin.board.Y12)] = (1, 8)
 
-    timer_dict[pyb.Pin.board.X1] = (2, 5)
-    timer_dict[pyb.Pin.board.X2] = (2, 5)
-    timer_dict[pyb.Pin.board.X3] = (2, 5, 9)
-    timer_dict[pyb.Pin.board.X4] = (2, 5, 9)
-    timer_dict[pyb.Pin.board.X5] = None
-    timer_dict[pyb.Pin.board.X6] = (2, 8)
-    timer_dict[pyb.Pin.board.X7] = (13)
-    timer_dict[pyb.Pin.board.X8] = (1, 8, 14)
-    timer_dict[pyb.Pin.board.X9] = (4)
-    timer_dict[pyb.Pin.board.X10] = (4)
-    timer_dict[pyb.Pin.board.X11] = None
-    timer_dict[pyb.Pin.board.X12] = None
-    timer_dict[pyb.Pin.board.X17] = None
-    timer_dict[pyb.Pin.board.X18] = None
-    timer_dict[pyb.Pin.board.X19] = None
-    timer_dict[pyb.Pin.board.X20] = None
-    timer_dict[pyb.Pin.board.X21] = None
-    timer_dict[pyb.Pin.board.X22] = None
+    timer_dict[str(pyb.Pin.board.X1)] = (2, 5)
+    timer_dict[str(pyb.Pin.board.X2)] = (2, 5)
+    timer_dict[str(pyb.Pin.board.X3)] = (2, 5, 9)
+    timer_dict[str(pyb.Pin.board.X4)] = (2, 5, 9)
+    timer_dict[str(pyb.Pin.board.X5)] = None
+    timer_dict[str(pyb.Pin.board.X6)] = (2, 8)
+    timer_dict[str(pyb.Pin.board.X7)] = (13)
+    timer_dict[str(pyb.Pin.board.X8)] = (1, 8, 14)
+    timer_dict[str(pyb.Pin.board.X9)] = (4)
+    timer_dict[str(pyb.Pin.board.X10)] = (4)
+    timer_dict[str(pyb.Pin.board.X11)] = None
+    timer_dict[str(pyb.Pin.board.X12)] = None
+    timer_dict[str(pyb.Pin.board.X17)] = None
+    timer_dict[str(pyb.Pin.board.X18)] = None
+    timer_dict[str(pyb.Pin.board.X19)] = None
+    timer_dict[str(pyb.Pin.board.X20)] = None
+    timer_dict[str(pyb.Pin.board.X21)] = None
+    timer_dict[str(pyb.Pin.board.X22)] = None
 
-    timer_dict[pyb.Pin.board.P2] = None
-    timer_dict[pyb.Pin.board.P3] = None
-    timer_dict[pyb.Pin.board.P4] = None
-    timer_dict[pyb.Pin.board.P5] = None
+    # timer_dict[str(pyb.Pin.board.P2)] = None
+    # timer_dict[str(pyb.Pin.board.P3)] = None
+    # timer_dict[str(pyb.Pin.board.P4)] = None
+    # timer_dict[str(pyb.Pin.board.P5)] = None
 
     """
     channel_dict: class varible for storing all the possible channel selections based upon pin
@@ -142,52 +143,52 @@ class TimerHelper:
     instantiation of this class.
     """
     channel_dict = {}
-    channel_dict[pyb.Pin.board.Y1] = (1)
-    channel_dict[pyb.Pin.board.Y2] = (2)
-    channel_dict[pyb.Pin.board.Y3] = (3, 1)
-    channel_dict[pyb.Pin.board.Y4] = (4, 1)
-    channel_dict[pyb.Pin.board.Y5] = None
-    channel_dict[pyb.Pin.board.Y6] = (1)
-    channel_dict[pyb.Pin.board.Y7] = (2, 2, 1)
-    channel_dict[pyb.Pin.board.Y8] = (3, 3, 2)
-    channel_dict[pyb.Pin.board.Y9] = (3)
-    channel_dict[pyb.Pin.board.Y10] = (4)
-    channel_dict[pyb.Pin.board.Y11] = (2, 2)
-    channel_dict[pyb.Pin.board.Y12] = (3, 3)
+    channel_dict[str(pyb.Pin.board.Y1)] = (1)
+    channel_dict[str(pyb.Pin.board.Y2)] = (2)
+    channel_dict[str(pyb.Pin.board.Y3)] = (3, 1)
+    channel_dict[str(pyb.Pin.board.Y4)] = (4, 1)
+    channel_dict[str(pyb.Pin.board.Y5)] = None
+    channel_dict[str(pyb.Pin.board.Y6)] = (1)
+    channel_dict[str(pyb.Pin.board.Y7)] = (2, 2, 1)
+    channel_dict[str(pyb.Pin.board.Y8)] = (3, 3, 2)
+    channel_dict[str(pyb.Pin.board.Y9)] = (3)
+    channel_dict[str(pyb.Pin.board.Y10)] = (4)
+    channel_dict[str(pyb.Pin.board.Y11)] = (2, 2)
+    channel_dict[str(pyb.Pin.board.Y12)] = (3, 3)
 
-    channel_dict[pyb.Pin.board.X1] = (1, 1)
-    channel_dict[pyb.Pin.board.X2] = (2, 2)
-    channel_dict[pyb.Pin.board.X3] = (3, 3, 1)
-    channel_dict[pyb.Pin.board.X4] = (4, 4, 2)
-    channel_dict[pyb.Pin.board.X5] = None
-    channel_dict[pyb.Pin.board.X6] = (1, 1)
-    channel_dict[pyb.Pin.board.X7] = (1)
-    channel_dict[pyb.Pin.board.X8] = (1, 1, 1)
-    channel_dict[pyb.Pin.board.X9] = (1)
-    channel_dict[pyb.Pin.board.X10] = (2)
-    channel_dict[pyb.Pin.board.X11] = None
-    channel_dict[pyb.Pin.board.X12] = None
-    channel_dict[pyb.Pin.board.X17] = None
-    channel_dict[pyb.Pin.board.X18] = None
-    channel_dict[pyb.Pin.board.X19] = None
-    channel_dict[pyb.Pin.board.X20] = None
-    channel_dict[pyb.Pin.board.X21] = None
-    channel_dict[pyb.Pin.board.X22] = None
+    channel_dict[str(pyb.Pin.board.X1)] = (1, 1)
+    channel_dict[str(pyb.Pin.board.X2)] = (2, 2)
+    channel_dict[str(pyb.Pin.board.X3)] = (3, 3, 1)
+    channel_dict[str(pyb.Pin.board.X4)] = (4, 4, 2)
+    channel_dict[str(pyb.Pin.board.X5)] = None
+    channel_dict[str(pyb.Pin.board.X6)] = (1, 1)
+    channel_dict[str(pyb.Pin.board.X7)] = (1)
+    channel_dict[str(pyb.Pin.board.X8)] = (1, 1, 1)
+    channel_dict[str(pyb.Pin.board.X9)] = (1)
+    channel_dict[str(pyb.Pin.board.X10)] = (2)
+    channel_dict[str(pyb.Pin.board.X11)] = None
+    channel_dict[str(pyb.Pin.board.X12)] = None
+    channel_dict[str(pyb.Pin.board.X17)] = None
+    channel_dict[str(pyb.Pin.board.X18)] = None
+    channel_dict[str(pyb.Pin.board.X19)] = None
+    channel_dict[str(pyb.Pin.board.X20)] = None
+    channel_dict[str(pyb.Pin.board.X21)] = None
+    channel_dict[str(pyb.Pin.board.X22)] = None
 
-    channel_dict[pyb.Pin.board.P2] = None
-    channel_dict[pyb.Pin.board.P3] = None
-    channel_dict[pyb.Pin.board.P4] = None
-    channel_dict[pyb.Pin.board.P5] = None
+    # channel_dict[str(pyb.Pin.board.P2)] = None
+    # channel_dict[str(pyb.Pin.board.P3)] = None
+    # channel_dict[str(pyb.Pin.board.P4)] = None
+    # channel_dict[str(pyb.Pin.board.P5)] = None
 
     # A dictionary to keep track of the timers that are available?
     timer_avail = {}
 
     def get_timer(self, pin, index=0):
         try:
-            timer_number = TimerHelper.timer_dict[pin][index]
-            timer_channel_number = TimerHelper.timer_channel[pin][index]
+            timer_number = TimerHelper.timer_dict[str(pin)][index]
+            timer_channel_number = TimerHelper.channel_dict[str(pin)][index]
         except TypeError:
-            timer_number = TimerHelper.timer_dict[pin]
-            timer_channel_number = TimerHelper.timer_channel[pin]
+            timer_number = TimerHelper.timer_dict[str(pin)]
+            timer_channel_number = TimerHelper.channel_dict[str(pin)]
 
         return (timer_number, timer_channel_number)
