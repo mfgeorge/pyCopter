4be3c9dd4dc2ef1c9d74502ad30900627fe5f862
Chad <clbickel@calpoly.edu>
2016-12-12 11:43:08 -0800

    Added tuning pictures

--- testing_data/pycopter_analyze.m
+++ testing_data/pycopter_analyze.m
@@ -8,7 +8,9 @@ clc
 
 filenames = {'pitch_log_p2high.csv','pitch_log_close2margstable.csv', ...
     'pitch_log_ponlytuned.csv','pitch_log_tunedWI.csv','pitch_log_Dadded.csv'};
-
+titles = {'P=0.8, I=0, D=0','P=0.4, I=0, D=0','P=0.2, I=0, D=0','P=0.2, I=0.01, D=0',....
+    'P=0.2, I=0.01, D = 0.05'};
+figure(1)
 for i = 1:length(filenames)
    filenames(i)
    A = csvread(char(filenames(i)));
@@ -21,10 +23,14 @@ for i = 1:length(filenames)
    time_ms(k) = NaN;
    time_ms = (time_ms - min(time_ms))/1000;
    hold on 
-   figure(i)
+   if i < 5
+       subplot(2,2,i)
+   else
+       figure(i)
+   end
    plot(time_ms,pitch); 
    xlabel('Time(s)')
    ylabel('Pitch(degrees)')
-    
+   title(char(titles(i)))
     
 end
